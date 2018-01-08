85d519971d7c49a196f5f0204851951a5251f375
slumduck <334176612@qq.com>
2018-01-09 00:33:26 +0800

    spring-boot log

--- src/main/java/com/mime/demo/seckill/BootStrap.java
+++ src/main/java/com/mime/demo/seckill/BootStrap.java
@@ -2,6 +2,8 @@ package com.mime.demo.seckill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -12,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 //@ComponentScan("com.mime.demo.seckill")
+@EnableCaching
 public class BootStrap {
 
     public static void main(String[] args) {
--- src/main/java/com/mime/demo/seckill/dao/UserMapper.java
+++ src/main/java/com/mime/demo/seckill/dao/UserMapper.java
@@ -1,10 +1,16 @@
 package com.mime.demo.seckill.dao;
 
 import com.mime.demo.seckill.entity.User;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
+@CacheConfig(cacheNames = "users")
 public interface UserMapper {
 
     /**
@@ -12,7 +18,21 @@ public interface UserMapper {
      * @param user
      * @return
      */
+    @CachePut(key = "#p0")
     int insert(User user);
+
+    //@Select("select * from user where id =#{id}")
+    @Cacheable(key ="#p0")
+    User findById(@Param("id") String id);
+
+    @CachePut(key = "#p0")
+    //@Update("update user set name=#{name} where id=#{id}")
+    void updataById(@Param("id")String id, @Param("name")String name);
+
+    //如果指定为 true，则方法调用后将立即清空所有缓存
+    @CacheEvict(key ="#p0",allEntries=true)
+    //@Delete("delete from user where id=#{id}")
+    void deleteById(@Param("id")String id);
 
 
 }
